37f37ba73ca2682c3d41feef318af2772e4192bd
Hexagons <anton@hexagons.se>
2019-10-12 15:38:34 +0200

    rotation

--- Shaders/EffectSingleTransformVOX.metal
+++ Shaders/EffectSingleTransformVOX.metal
@@ -13,6 +13,9 @@ struct Uniforms{
     float tx;
     float ty;
     float tz;
+    float rx;
+    float ry;
+    float rz;
     float s;
     float sx;
     float sy;
@@ -28,14 +31,38 @@ kernel void effectSingleTransformVOX(const device Uniforms& in [[ buffer(0) ]],
     if (pos.x >= outTex.get_width() || pos.y >= outTex.get_height() || pos.z >= outTex.get_depth()) {
       return;
     }
+    
+    float pi = 3.14159265359;
         
     float x = float(pos.x + 0.5) / float(outTex.get_width());
     float y = float(pos.y + 0.5) / float(outTex.get_height());
     float z = float(pos.z + 0.5) / float(outTex.get_depth());
     
     float3 size = float3(in.sx * in.s, in.sy * in.s, in.sz * in.s);
-    float3 p = float3(x + in.tx, y + in.ty, z + in.tz);
-    float3 uvw = (p - 0.5) / size + 0.5;
+    float3 p = float3(x + in.tx - 0.5, y + in.ty - 0.5, z + in.tz - 0.5);
+    
+    if (in.rx != 0.0) {
+        float angx = atan2(p.z, p.y) + (-in.rx * pi * 2);
+        float ampx = sqrt(pow(p.y, 2) + pow(p.z, 2));
+        float2 rotx = float2(cos(angx) * ampx, sin(angx) * ampx);
+        p = float3(p.x, rotx[0], rotx[1]);
+    }
+    
+    if (in.ry != 0.0) {
+        float angy = atan2(p.z, p.x) + (-in.ry * pi * 2);
+        float ampy = sqrt(pow(p.x, 2) + pow(p.z, 2));
+        float2 roty = float2(cos(angy) * ampy, sin(angy) * ampy);
+        p = float3(roty[0], p.y, roty[1]);
+    }
+    
+    if (in.rz != 0.0) {
+        float angz = atan2(p.y, p.x) + (-in.rz * pi * 2);
+        float ampz = sqrt(pow(p.x, 2) + pow(p.y, 2));
+        float2 rotz = float2(cos(angz) * ampz, sin(angz) * ampz);
+        p = float3(rotz[0], rotz[1], p.z);
+    }
+    
+    float3 uvw = p / size + 0.5;
     
     float4 c = inTex.sample(s, uvw);
     
--- Source/VOX/VOXs/Effects/Single/TransformVOX.swift
+++ Source/VOX/VOXs/Effects/Single/TransformVOX.swift
@@ -1,6 +1,6 @@
 //
 //  TransformVOX.swift
-//  PixelKit
+//  VoxelKit
 //
 //  Created by Hexagons on 2018-08-28.
 //  Open Source - MIT License
@@ -16,13 +16,14 @@ public class TransformVOX: VOXSingleEffect {
     // MARK: - Public Properties
     
     public var position: LiveVec = .zero
+    public var rotation: LiveVec = .zero
     public var scale: LiveFloat = LiveFloat(1.0, max: 2.0)
     public var size: LiveVec = LiveVec(x: 1.0, y: 1.0, z: 1.0)
     
     // MARK: - Property Helpers
     
     override public var liveValues: [LiveValue] {
-        return [position, scale, size]
+        return [position, rotation, scale, size]
     }
     
     // MARK: - Life Cycle
@@ -37,31 +38,49 @@ public class TransformVOX: VOXSingleEffect {
 public extension NODEOut {
     
     func _move(by position: LiveVec) -> TransformVOX {
-        let transformPix = TransformVOX()
-        transformPix.name = "position:transform"
-        transformPix.input = self as? VOX & NODEOut
-        transformPix.position = position
-        return transformPix
+        let transformVox = TransformVOX()
+        transformVox.name = "position:transform"
+        transformVox.input = self as? VOX & NODEOut
+        transformVox.position = position
+        return transformVox
     }
     
     func _move(x: LiveFloat = 0.0, y: LiveFloat = 0.0, z: LiveFloat = 0.0) -> TransformVOX {
         return (self as! VOX & NODEOut)._move(by: LiveVec(x: x, y: y, z: z))
     }
     
+    func _rotatate(by rotation: LiveFloat, on axis: Axis) -> TransformVOX {
+        let transformVox = TransformVOX()
+        transformVox.name = "rotatate:transform"
+        transformVox.input = self as? VOX & NODEOut
+        transformVox.rotation = LiveVec(x: axis == .x ? rotation : 0.0,
+                                        y: axis == .y ? rotation : 0.0,
+                                        z: axis == .z ? rotation : 0.0)
+        return transformVox
+    }
+    
+    func _rotatate(by360 rotation: LiveFloat, on axis: Axis) -> TransformVOX {
+        return (self as! VOX & NODEOut)._rotatate(by: rotation / 360, on: axis)
+    }
+    
+    func _rotatate(by2pi rotation: LiveFloat, on axis: Axis) -> TransformVOX {
+        return (self as! VOX & NODEOut)._rotatate(by: rotation / (.pi * 2), on: axis)
+    }
+    
     func _scale(by scale: LiveFloat) -> TransformVOX {
-        let transformPix = TransformVOX()
-        transformPix.name = "scale:transform"
-        transformPix.input = self as? VOX & NODEOut
-        transformPix.scale = scale
-        return transformPix
+        let transformVox = TransformVOX()
+        transformVox.name = "scale:transform"
+        transformVox.input = self as? VOX & NODEOut
+        transformVox.scale = scale
+        return transformVox
     }
     
     func _scale(size: LiveVec) -> TransformVOX {
-        let transformPix = TransformVOX()
-        transformPix.name = "scale:transform"
-        transformPix.input = self as? VOX & NODEOut
-        transformPix.size = size
-        return transformPix
+        let transformVox = TransformVOX()
+        transformVox.name = "scale:transform"
+        transformVox.input = self as? VOX & NODEOut
+        transformVox.size = size
+        return transformVox
     }
     
     func _scale(x: LiveFloat = 1.0, y: LiveFloat = 1.0, z: LiveFloat = 1.0) -> TransformVOX {
